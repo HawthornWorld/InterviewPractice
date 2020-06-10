213578ac9ed8b7486fe42fd520421a8d2b4d19f4
ymaa <ymaa@ctrip.com>
2020-06-10 23:21:45 +0800

    levelOrder&&levelOrderWithSplit

--- run.js
+++ run.js
@@ -1,5 +1,5 @@
-Array.prototype.toString.call([1,2,3])
-Array.prototype.toString.call({a:1,b:2,c:3})
+Array.prototype.toString.call([1, 2, 3]);
+Array.prototype.toString.call({ a: 1, b: 2, c: 3 });
 
 /**
  * 实现map
@@ -20,7 +20,7 @@ Array.prototype.myMap = function (fn, thisArg) {
 		);
 		res.push(tmp);
 	}
-	console.log("res11", res);
+	// console.log("res11", res);
 	return res;
 };
 
@@ -43,7 +43,7 @@ function fn0(cur, idx) {
 	return cur;
 }
 let ressss = arr0.myMapWithReduce(fn0);
-console.log("ressss", ressss);
+// console.log("ressss", ressss);
 
 /**
  * forEach
@@ -193,11 +193,11 @@ function fib(n) {
 	for (let i = 2; i < n; i++) {
 		arr[i] = arr[i - 2] + arr[i - 1];
 	}
-	console.log(arr);
+	// console.log(arr);
 	return arr[n - 1];
 }
 let fib1 = fib(8);
-console.log(fib1);
+// console.log(fib1);
 // 递归写法
 function fib2(n) {
 	if (n <= 1) {
@@ -313,23 +313,23 @@ function debounce(fn = () => {}, delay) {
  * curry
  * 只传递给函数一部分参数来调用它，让它返回一个函数去处理剩下的参数。
  * */
-function myCurry(fn) {
-	let argLen = fn.length;
-	let args = [];
-	let self = this;
-	return function () {
-		args = args.concat(Array.prototype.slice.call(arguments));
-		if (args.length < argLen) {
-			return arguments.callee;
-		}
-		return fn.apply(self, args);
-	};
-}
-const add = function (a, b, c, d) {
-	return a + b + c + d;
-};
-const res = myCurry(add);
-res(a)(b)(c)(d);
+// function myCurry(fn) {
+// 	let argLen = fn.length;
+// 	let args = [];
+// 	let self = this;
+// 	return function () {
+// 		args = args.concat(Array.prototype.slice.call(arguments));
+// 		if (args.length < argLen) {
+// 			return arguments.callee;
+// 		}
+// 		return fn.apply(self, args);
+// 	};
+// }
+// const add = function (a, b, c, d) {
+// 	return a + b + c + d;
+// };
+// const res = myCurry(add);
+// res(a)(b)(c)(d);
 /**
  * sleep
  * */
@@ -405,111 +405,261 @@ res(a)(b)(c)(d);
 // })()
 
 // 字节题目一：
-inner = "window";
-function say() {
-	console.log(inner);
-	console.log(this.inner);
+// inner = "window";
+// function say() {
+// 	console.log(inner);
+// 	console.log(this.inner);
+// }
+// var obj1 = (function () {
+// 	var inner = "1-1";
+// 	return {
+// 		inner: "1-2",
+// 		say: function () {
+// 			console.log(inner);
+// 			console.log(this.inner);
+// 		},
+// 	};
+// })();
+
+// var obj1 = {
+// 	inner: "1-2",
+// 	say: function () {
+// 		console.log(inner);
+// 		console.log(this.inner);
+// 	},
+// };
+
+// var obj2 = (function () {
+// 	var inner = "2-1";
+// 	return {
+// 		inner: "2-2",
+// 		say: function () {
+// 			console.log(inner);
+// 			console.log(this.inner);
+// 		},
+// 	};
+// })();
+
+// say();
+// // 'window'
+// // 'window'
+// obj1.say();
+// // '1-1'
+// // '1-2'
+// obj2.say();
+// // '2-1'
+// // '2-2'
+// obj1.say = say;
+// obj1.say();
+// // 'window'
+// // '1-2'
+// obj1.say = obj2.say;
+// obj1.say();
+// // '2-1'
+// // '1-2'
+
+// var a = 10010;
+
+// var obj = {
+// 	a: 10086,
+// 	fn: function () {
+// 		console.log(this);
+// 		console.log(this.a);
+// 	},
+// };
+
+// obj.fn();
+
+// let foo = obj.fn;
+// foo();
+
+// var obj3 = {
+// 	a: 10000,
+// 	fn: function () {
+// 		console.log(this);
+// 		console.log(this.a);
+// 	},
+// };
+
+// obj.fn = obj3.fn;
+// obj.fn();
+// obj3.fn();
+
+// function Foo() {
+// 	getName = function () {
+// 		console.log(1);
+// 	};
+// 	return this;
+// }
+// Foo.getName = function () {
+// 	console.log(2);
+// };
+// Foo.prototype.getName = function () {
+// 	console.log(3);
+// };
+// var getName = function () {
+// 	console.log(4);
+// };
+// function getName() {
+// 	console.log(5);
+// }
+
+// //答案：
+// Foo.getName(); //2
+// getName(); //4
+// // Foo().getName(); //4
+// getName(); //4
+// new Foo.getName(); //2
+// new Foo().getName(); // 3
+// console.dir("new foo()", new Foo());
+// new new Foo().getName(); // 3
+
+// 被问的查找方面的算法：
+// 查找树的所有节点
+// 查找页面dom中一共使用了多少种ID,以及每个id被使用的次数
+// 二叉树的遍历方式有哪些
+
+// function levelOrder(node) {
+// 	let queue = [];
+// 	const res = [];
+// 	if (root !== null) {
+// 		let node = queue.push(root);
+// 		while (queue.length > 0) {
+// 			let node = queue.shift();
+// 			res.push(node);
+// 			if (node.children.length > 0) {
+// 				node.children.forEach((item) => {
+// 					queue.push(item);
+// 				});
+// 			}
+// 		}
+// 	}
+// 	console.log('res', res)
+// }
+
+// 给定一个 N 叉树，返回其节点值的层序遍历。 (即从左到右，逐层遍历)。
+// 例如三叉树返回 [
+// 	[1],
+// 	[3,2,4],
+// 	[5,6]
+// ]
+// 关键点；如何分层
+// function levelOrder2(root) {
+// 	let queue = [];
+// 	const res = [];
+// 	if (root !== null) {
+// 		let node = queue.push(root);
+// 		let floorIdx = 0;
+// 		res[0] = [root];
+// 		let isFirst = true;
+// 		while (queue.length > 0) {
+// 			if(isFirst) {
+// 				floorIdx++;
+// 				res[floorIdx] = [];
+// 			}
+// 			let node = queue.shift();
+// 			res[floorIdx].push(node);
+// 			if(queue.length === 0) {
+// 				floorIdx++;
+// 				res[floorIdx] = [];
+// 			}
+// 			if (node.children.length > 0) {
+// 				node.children.forEach((item) => {
+// 					queue.push(item);
+// 				});
+// 			}
+// 		}
+// 	}
+// 	console.log('res', res)
+// }
+
+// class Node {
+// 	constructor(val) {
+// 		this.val = val;
+// 		this.children = [];
+// 	}
+// }
+
+// let root = new Node(1);
+
+// let r1 = new Node(2);
+// let r2 = new Node(3);
+// let r3 = new Node(4);
+
+// root.children.push(r1);
+// root.children.push(r2);
+// root.children.push(r3);
+
+// let r4 = new Node(5);
+
+// r2.children.push(r4);
+
+// levelOrder2(root);
+
+function levelOrder(root) {
+	let queue = [];
+	let res = [];
+	if (root !== null) {
+		queue.push(root);
+		while (queue.length > 0) {
+			let node = queue.shift();
+			res.push(node);
+			if (node.children.length > 0) {
+				node.children.forEach((item) => {
+					queue.push(item);
+				});
+			}
+		}
+	}
+	console.log("***res****", res);
 }
-var obj1 = (function () {
-	var inner = "1-1";
-	return {
-		inner: "1-2",
-		say: function () {
-			console.log(inner);
-			console.log(this.inner);
-		},
-	};
-})();
 
-var obj1 = {
-	inner: "1-2",
-	say: function () {
-		console.log(inner);
-		console.log(this.inner);
-	},
-};
-
-var obj2 = (function () {
-	var inner = "2-1";
-	return {
-		inner: "2-2",
-		say: function () {
-			console.log(inner);
-			console.log(this.inner);
-		},
-	};
-})();
-
-say();
-// 'window'
-// 'window'
-obj1.say();
-// '1-1'
-// '1-2'
-obj2.say();
-// '2-1'
-// '2-2'
-obj1.say = say;
-obj1.say();
-// 'window'
-// '1-2'
-obj1.say = obj2.say;
-obj1.say();
-// '2-1'
-// '1-2'
-
-var a = 10010;
-
-var obj = {
-	a: 10086,
-	fn: function () {
-		console.log(this);
-		console.log(this.a);
-	},
-};
-
-obj.fn();
-
-let foo = obj.fn;
-foo();
-
-var obj3 = {
-	a: 10000,
-	fn: function () {
-		console.log(this);
-		console.log(this.a);
-	},
-};
-
-obj.fn = obj3.fn;
-obj.fn();
-obj3.fn();
-
-function Foo() {
-	getName = function () {
-		console.log(1);
-	};
-	return this;
-}
-Foo.getName = function () {
-	console.log(2);
-};
-Foo.prototype.getName = function () {
-	console.log(3);
-};
-var getName = function () {
-	console.log(4);
-};
-function getName() {
-	console.log(5);
+function levelOrderWithSplit(root) {
+	if (root !== null) {
+		// Array[number[]]
+		let res = [];
+		let queue = [];
+		queue.push(root);
+		while (queue.length > 0) {
+			let size = queue.length;
+			let tmpLevel = [];
+			while (size > 0) {
+				let node = queue.shift();
+				tmpLevel.push(node.val);
+				if (node.children.length > 0) {
+					node.children.forEach((item) => {
+						queue.push(item);
+					});
+				}
+				size--;
+			}
+			res.push(tmpLevel);
+		}
+		console.log('---res---',res);
+		return res;
+	} else {
+		return [];
+	}
 }
 
-//答案：
-Foo.getName(); //2
-getName(); //4
-// Foo().getName(); //4
-getName(); //4
-new Foo.getName(); //2
-new Foo().getName(); // 3
-console.dir("new foo()", new Foo());
-new new Foo().getName(); // 3
+class Node {
+	constructor(val) {
+		this.val = val;
+		this.children = [];
+	}
+}
+let root = new Node(1);
+let r1 = new Node(2);
+let r2 = new Node(3);
+let r3 = new Node(4);
+root.children.push(r1);
+root.children.push(r2);
+root.children.push(r3);
+
+let r4 = new Node(5);
+r3.children.push(r4);
+
+levelOrderWithSplit(root);
+
+
+
