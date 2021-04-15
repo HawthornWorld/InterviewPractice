a5f5dfa4d04b5f32b7fc58ea3c97abb30a930deb
mayan753 <mayan753@hellobike.com>
2021-04-15 19:51:06 +0800

    update

--- CODE/hello.js
+++ CODE/hello.js
@@ -28,7 +28,27 @@
 // console.log(c); // 1
 
 
-let res = false;
-if(res) {
-	console.log(111)
-}
+// 需要注意的是，立即resolve的 Promise 对象，是在本轮“事件循环”（event loop）的结束时执行执行，不是马上执行,也不是在下一轮“事件循环”的开始时执行
+// 原因：传递到 then() 中的函数被置入了一个微任务队列，而不是立即执行，这意味着它是在 JavaScript 事件队列的所有运行时结束了，事件队列被清空之后，才开始执行
+Promise.resolve().then(()=>{
+    console.log(0);
+    setTimeout(()=> {
+        console.log(4.5)
+    })
+    return Promise.resolve(4);// return 4
+}).then((res)=>{
+    console.log(res)
+});
+
+Promise.resolve().then(()=>{
+    console.log(1);
+}).then(()=>{
+    console.log(2);
+}).then(()=>{
+    console.log(3);
+}).then(()=>{
+    console.log(5);
+}).then(()=>{
+    console.log(6);
+});
+	
